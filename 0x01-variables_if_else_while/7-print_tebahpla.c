e408c78c7e1cf92042d981a29a32c0811ea7a4ef
DMBAKA <damambaka012@gmail.com>
2022-06-05 06:33:19 -0700

    mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main prints the lowercase alphabet in reverse
+*
+* Always: Return 0
+*/
+int main(void)
+{
+	char lowcase;
+
+	for (lowcase = 'z'; lowcase >= 'a'; lowcase--)
+	putchar(lowcase);
+putchar('\n');
+return (0);
+}
