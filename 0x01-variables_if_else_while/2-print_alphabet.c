d22fb880fd128f3345855ff4b61c9d906d6a5ffc
DMBAKA <damambaka012@gmail.com>
2022-06-04 23:55:15 -0700

    lowcase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
-/**                                                                                                                           
-* main - Prints the alphabet in lowercase.                                                                                    *
-* Return: Always 0.  
-*/                                                                                                               
+/**
+* main - Prints the alphabet in lowercase.
+*
+* Return: Always 0.
+*/
 int main(void)
 {
-char lowcase;                                                                                       
-for (lowcase = 'a'; lowcase <= 'z'; lowcase++)                                                                                putchar(lowcase);                                                                             
-putchar('\n');                                                                                                           
-return (0);                                                                                                                   
+char lowcase;
+for (lowcase = 'a'; lowcase <= 'z'; lowcase++)
+putchar(lowcase);
+putchar('\n');
+return (0);
 }
