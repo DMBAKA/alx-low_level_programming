341bcca15e0fbfcbe07b0b5d98e4d61a35b8ecb2
DMBAKA <damambaka012@gmail.com>
2022-06-05 00:51:46 -0700

    missing e q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+*main- print lowercase except e and q
+*
+* Return: Always 0
+*/
+int main(void)
+{char lowcase;
+	for (lowcase = 'a'; lowcase <='z'; lowcase++)
+if (lowcase != 'e' && lowcase != 'q')
+putchar (lowcase);
+putchar ('\n');
+return (0);
+}
+
