a7da65ce31eefc00ea5c972b593f14af351fe6a4
DMBAKA <damambaka012@gmail.com>
2022-06-07 13:04:52 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include<main.h>
+/**
+*
+* main - check the code
+*
+* Return: Always 0
+*/
+int main(void)
+{
+	char lowcase;
+
+	for (lowcase = 'a'; lowcase <= 'z'; lowcase++)
+
+putchar('lowcase');
+putchar('\n');
+print_alphabet();
+return (0);
+}
