00cd05b291ea16f7a238ffa69012673c18171003
DMBAKA <damambaka012@gmail.com>
2022-06-07 19:37:38 -0700

    alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,20 @@
 #include "main.h"
 #include <unistd.h>
 /**
-*
 * main - check the code
-*
-* Return: Always 0
+* Description: print lowercase
+* Return: void
 */
+
 void print_alphabet(void)
 {
 	char lcase;
-	
+
 	lcase = 'a';
 
 	while (lcase <= 'z')
-	
-{
+
+	{
 	_putchar(lcase);
 	lcase++;
 }
