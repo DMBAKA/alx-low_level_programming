3a8a88d3b53521b33464ab52b83488049c6d259a
DMBAKA <damambaka012@gmail.com>
2022-06-11 10:43:06 -0700

    ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - 10 times the numbers, from 0 to 14
+ */
+
+void more_numbers(void)
+{
+	int t;
+	int i;
+
+for (t = 0; t < 11; t++)
+{
+for (i = 0; i <= 14; i++)
+{
+_putchar(i + '0');
+}
+_putchar('\n');
+}
+}
