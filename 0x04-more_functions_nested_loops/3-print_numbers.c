6c3eaa588aff9b9870428ca1de45eff596c11adf
DMBAKA <damambaka012@gmail.com>
2022-06-11 10:19:07 -0700

    number

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,13 +5,13 @@
  * print_numbers - prints the numbers, from 0 to 9, followed by a new line
  * Return: Always 0
  */
-void print_numbers(void);
+void print_numbers(void)
 {
-char i;
+int i;
 
-for(i = 48; i < 58; i++)
+for (i = 0; i < 10; i++)
 {
-_putchar(i);
+_putchar(i + '0');
 }
 _putchar('\n');
 }
