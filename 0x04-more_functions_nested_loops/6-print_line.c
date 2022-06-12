0c9712bd2995aecfed94dcda5600deaca396424e
DMBAKA <damambaka012@gmail.com>
2022-06-12 01:17:40 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal.
+ * @n: size of line
+ */
+void print_line(int n)
+{
+if (n >= 1)
+_putchar('_');
+else
+_putchar('\n');
+}
