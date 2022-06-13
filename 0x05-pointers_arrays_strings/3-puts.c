44ba738ad95ceae3132373374ac1c3d4d9dd5003
DMBAKA <damambaka012@gmail.com>
2022-06-13 11:16:24 -0700

    string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @*str: Character str input
+ * main - check the code
+ * Return: Always 0
+ */
+
+void _puts(char *str)
+{
+
+	puts(str);
+}
